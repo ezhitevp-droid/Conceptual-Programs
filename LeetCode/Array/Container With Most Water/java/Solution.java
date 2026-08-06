8b57957e59a547b4c2c41f51d9eb5dee8644a801
Vishnu priyan S <ezhitevp@gmail.com>
2026-08-06 10:10:05 -0700

    feat: Add Container With Most Water

---
+++ LeetCode/Array/Container With Most Water/java/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea=0;
+        int l=0;
+        int r=height.length-1;
+    
+        while(l<r){
+            maxArea=Math.max(maxArea,(r-l)*Math.min(height[l],height[r]));
+            //int sum=arr[l]+arr[r];
+            if(height[l]<height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return maxArea;
+    }
+}
